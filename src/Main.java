99b419e266193a2fbf39de0e32d118298264d7ed
ПристрійUNICEF <twinkgenshartem@gmail.com>
2024-12-07 14:18:27 +0200

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import figures.Rectangle;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        int square = rectangle.getSquare();
+    }
+}
+
---
+++ src/figures/Rectangle.java
@@ -0,0 +1,13 @@
+package figures;
+
+public class Rectangle {
+    int x;
+    int y;
+    public Rectangle(int storonal, int storonal2) {
+        x = storonal;
+        y = storonal2;
+    }
+    public void getSquare() {
+        x
+    }
+}
